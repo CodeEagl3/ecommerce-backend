191f9f6af39553a039b01824f6578ea1f4f4318f
CodeEagl3 <oluwasomidotun7@gmail.com>
2021-02-23 19:39:12 +0100

    backend initialized

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0fb44a620bb474eea2922e6836da096a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2fd94feb-dbae-4da8-88f4-490e18c1475c'
+};
